c6d63ecae9ca4aed8217d81f7d761dc3c566c133
ram8528 <106538883+ram8528@users.noreply.github.com>
2025-09-14 00:03:17 +0530

    Add validation schemas for message acceptance, sign-in, sign-up, and verification

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,10 @@
+import {z} from 'zod'
+
+export const usernameValidation = z.string().min(2,"username must at least 2 characters").max(20,"username cannot be more than 20 characters").regex(/^[a-zA-Z0-9_]+$/,"username must not contain special characters")
+
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: z.string().email({message: 'Invalid email address'}),
+    password: z.string().min(6,{message: "Password must be at least 6 charcters"})
+})
