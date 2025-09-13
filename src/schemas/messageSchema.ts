b442e250c366749ae794cfa00e7ad53a480cc38c
ram8528 <106538883+ram8528@users.noreply.github.com>
2025-09-14 00:12:50 +0530

    Initial commit

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,6 +1,5 @@
 import {z} from "zod" 
 
 export const MessageSchema = z.object({
-    content: z.string().min(10,{message: 'content must be at least 10 characters'}).max(300,{message: 'content must be no longer than 300 characters'})
-    
-})
+  content: z.string().min(10, { message: 'content must be at least 10 characters' })
+});
