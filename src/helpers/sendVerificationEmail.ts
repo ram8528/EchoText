9f1160d3d042eb19070f8260e2883dc5bbac3990
ram8528 <106538883+ram8528@users.noreply.github.com>
2025-09-18 01:16:59 +0530

    integrate next-auth for user authentication and add middleware for protected routes

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,6 +1,6 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
-import { ApiResponse } from "../../types/ApiResponse";
+import { ApiResponse } from "../types/ApiResponse";
 
 export async function sendVerificationEmail(
   email: string,
